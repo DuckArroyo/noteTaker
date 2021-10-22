7d8fcf044b567cfeb79391c2a997cbf3ea237ed0
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-10-22 14:46:04 -0700

    stable code, cannot figure out connecting db

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 const express = require("express");
+const path = require("path");
+//const { db } = require("./db/db.json");
 
+const PORT = process.env.PORT || 3001;
 //Initiates server
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+  res.send("Hello!");
+});
+
+// app.get("/api/notes/:id", (req, res) => {
+//   const result = findById(req.params.id, db);
+//   if (result) {
+//     res.json(result);
+//   } else {
+//     res.send(404);
+//   }
+// });
+
+//   app.post('/api/notes', (req, res) => {
+//     // set id based on what the next index of the array will be
+//     req.body.id = animals.length.toString();
+
+//     if (!validateAnimal(req.body)) {
+//       res.status(400).send('The note is incomplete.');
+//     } else {
+//       const animal = createNewAnimal(req.body, db);
+//       res.json(animal);
+//     }
+//   });
+
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+//   });
+
+//   app.get('/animals', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/animals.html'));
+//   });
+
+//   app.get('/zookeepers', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+//   });
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'));
+//   });
+
 //Check that the server is listening and running
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
