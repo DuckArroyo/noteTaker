42d7a6d4a9174065f04156c5448b257eaf744e9f
DuckArroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-10-22 16:37:44 -0700

    switching computers

--- server.js
+++ server.js
@@ -16,42 +16,42 @@ app.get("/api/notes", (req, res) => {
   res.json(db);
 });
 
-// app.get("/api/notes/:id", (req, res) => {
-//   const result = findById(req.params.id, db);
-//   if (result) {
-//     res.json(result);
-//   } else {
-//     res.send(404);
-//   }
-// });
+app.get("/api/notes/:id", (req, res) => {
+  const result = findById(req.params.id, db);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
-//   app.post('/api/notes', (req, res) => {
-//     // set id based on what the next index of the array will be
-//     req.body.id = animals.length.toString();
+app.post("/api/notes", (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
 
-//     if (!validateAnimal(req.body)) {
-//       res.status(400).send('The note is incomplete.');
-//     } else {
-//       const animal = createNewAnimal(req.body, db);
-//       res.json(animal);
-//     }
-//   });
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The note is incomplete.");
+  } else {
+    const animal = createNewAnimal(req.body, db);
+    res.json(animal);
+  }
+});
 
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-//   });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-//   app.get('/animals', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/animals.html'));
-//   });
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
 
-//   app.get('/zookeepers', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-//   });
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-//   });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 //Check that the server is listening and running
 app.listen(PORT, () => {
